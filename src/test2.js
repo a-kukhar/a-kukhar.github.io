5a5298514311897f9d990a1780547a1b182e7977
Anton Kukhar <anton.kukhar@gmail.com>
2020-03-13 16:30:11 +0200

    Add files via upload

--- src/test2.js
+++ src/test2.js
@@ -4,14 +4,15 @@ TestScene2 = function() {
 
 TestScene2.prototype.create = function() {
     console.log("Preloader scene function");
-    this.load.on("complete", this.loadComplete, this);
+    /*this.load.on("complete", this.loadComplete, this);
     this.load.setPath("./images/theme_01/")
     
     this.load.image("cell", "cell.png");
     this.load.image("ball-1", "ball-1.png");
     this.load.image("ball-2", "ball-2.png");
 
-    this.load.start();
+    this.load.start();*/
+    this.scene.start("Ready");
 };
 
 TestScene2.prototype.loadComplete = function() {
