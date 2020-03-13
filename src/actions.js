0bff54bd945ce538db061024f22c9d2350f8dded
Anton Kukhar <anton.kukhar@gmail.com>
2020-03-13 16:03:49 +0200

    Add files via upload

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,18 @@
+/**
+ * Handlers for GameShell events. Names is hardcoded in gameshell.js
+ */
+
+/**
+ * Get "welcome" message with general information about players.
+ * @param {GameShellInfo} data 
+ */
 function setGameshellInfoHook(data) {
-    debugger;
+    GAMESHELL_PLAYERS_INFO        = data;
+    GAMESHELL_CURRENT_PLAYER      = data.currentPlayer;
+    GAMESHELL_CURRENT_PLAYER_TYPE = data.userType;
 };
 
 function startGameHook() {
+    PHASER_GAME
     debugger;
 }
